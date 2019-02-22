dba08ee9a35053d31782388da00b842912973bd5
Julian WEBER <julian.weber@hotmail.fr>
2019-02-22 14:01:10 +0100

    clean code

--- js/validation.js
+++ js/validation.js
@@ -1,24 +1,4 @@
 module.exports = {
-    goodSizes: (slices, [R, C, L, H]) => {
-        for (let i = 0; i < slices.length; i++){
-            let slice = slices[i]
-            if(!(
-                slice[0] >= 0 && 
-                slice[1] >= 0 &&
-                slice[2] >= 0 && 
-                slice[3] >= 0 &&
-                slice[0] < R &&
-                slice[1] < C &&
-                slice[2] < R &&
-                slice[3] < C &&
-                (slice[2]-slice[0]+1) * (slice[3]-slice[1]+1) >= 2*L
-                (slice[2]-slice[0]+1) * (slice[3]-slice[1]+1) <= H
-            )){
-                return false
-            }
-        }
-        return true
-    },
     goodSize: (slice, [R, C, L, H]) => {
         if(!(
             slice[0] >= 0 && 
@@ -37,25 +17,6 @@ module.exports = {
         else{
             return true
         }
-    },
-    noOverlapsBatch: (slices, slice) => {
-        for(let i = 0; i < slices.length; i++){
-            let over = false
-            let left = false
-            
-            if(slice[1] > slices[i][3] || slices[i][1] > slice[3]){
-                left = true
-            }
-
-            if(slice[2] < slices[i][0] || slices[i][2] < slice[0]){
-                over = true
-            }
-
-            if (!(over || left)){
-                return false
-            }      
-        }
-        return true
     },
     noOverlaps: (slices, slice) => {
         for(let i = 0; i < slices.length; i++){
